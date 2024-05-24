93f89628cdcf3100ed5d3772395495462d2776fd
Janaina Voigt <jv.de@icloud.com>
2024-05-24 17:08:35 +0200

    Added the test of ArtPieces ande ArtPiecesPreview components

--- pages/components/ArtPiecePreview.js
+++ pages/components/ArtPiecePreview.js
@@ -1,1 +1,11 @@
-export default function ArtPiecePreview({ image, title, artist }) {}
+import Image from "next/image";
+
+export default function ArtPiecePreview({ image, title, artist }) {
+  return (
+    <>
+      <Image src={image} alt={title} width={300} height={300} />
+      <h2>{title}</h2>
+      <p>Artist: {artist}</p>
+    </>
+  );
+}
--- pages/components/ArtPieces.js
+++ pages/components/ArtPieces.js
@@ -1,22 +1,17 @@
-import Image from "next/image";
+import ArtPiecePreview from "./ArtPiecePreview";
 
 export default function ArtPieces({ pieces }) {
   return (
     <div>
-      <ul>
-        {pieces?.map((piece) => (
-          <li key={piece.slug}>
-            <Image
-              src={piece.imageSource}
-              alt={piece.slu}
-              width={300}
-              height={300}
-            />
-            <h2>{piece.name}</h2>
-            <h3>Artist: {piece.artist}</h3>
-          </li>
-        ))}
-      </ul>
+      {pieces?.map((piece) => (
+        <li key={piece.slug}>
+          <ArtPiecePreview
+            image={piece.imageSource}
+            title={piece.name}
+            artist={piece.artist}
+          />
+        </li>
+      ))}
     </div>
   );
 }
