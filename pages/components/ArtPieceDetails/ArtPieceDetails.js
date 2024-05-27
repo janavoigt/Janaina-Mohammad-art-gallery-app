283382c31bb6a4711ebddac98d8f26cc91dbe76b
Janaina Voigt <jv.de@icloud.com>
2024-05-27 23:35:19 +0200

    Adittional steps

--- pages/components/ArtPieceDetails/ArtPieceDetails.js
+++ pages/components/ArtPieceDetails/ArtPieceDetails.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import FavoriteButton from "../FavoriteButton/FavoriteButton";
+import CommentForm from "../CommentForm/CommentForm";
+import { useArtPiecesStore } from "@/stores/userPiecesStores";
+import Comments from "../Comments/Comments";
 
 export default function ArtPieceDetails({
   image,
@@ -9,6 +12,9 @@ export default function ArtPieceDetails({
   genre,
   slug,
 }) {
+  const comments = useArtPiecesStore((state) => state.getComments(slug));
+  const addComment = useArtPiecesStore((state) => state.addComment);
+
   return (
     <>
       <article>
@@ -22,6 +28,10 @@ export default function ArtPieceDetails({
         </ul>
       </article>
       <button type="button">Back to the List</button>
+      <Comments comments={comments} />
+      <CommentForm
+        onSubmitComment={(data) => addComment(slug, data.comment, Date.now())}
+      />
     </>
   );
 }
--- stores/userPiecesStores.js
+++ stores/userPiecesStores.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { produce } from "immer";
 
 export const useArtPiecesStore = create((set, get) => ({
   artPieces: [],
@@ -16,7 +17,7 @@ export const useArtPiecesStore = create((set, get) => ({
         return {
           artPiecesInfo: [
             ...state.artPiecesInfo,
-            { slug: slug, isFavorite: true },
+            { slug: slug, isFavorite: true, comments: [] },
           ],
         };
       } else {
@@ -34,5 +35,42 @@ export const useArtPiecesStore = create((set, get) => ({
       .map((piece) => piece.slug);
 
     return get().artPieces.filter((piece) => favorites.includes(piece.slug));
+  },
+  addComment: (slug, comment, timestamp) => {
+    set((state) => {
+      const info = state.artPiecesInfo.find((piece) => piece.slug === slug);
+      if (info) {
+        const otherInfos = state.artPiecesInfo.filter(
+          (piece) => piece.slug !== slug
+        );
+        return {
+          artPiecesInfo: [
+            ...otherInfos,
+            {
+              ...info,
+              comments: [
+                ...info.comments,
+                { comment: comment, timestamp: timestamp },
+              ],
+            },
+          ],
+        };
+      } else {
+        return {
+          artPiecesInfo: [
+            ...state.artPiecesInfo,
+            {
+              slug: slug,
+              isFavorite: false,
+              comments: [{ comment: comment, timestamp: timestamp }],
+            },
+          ],
+        };
+      }
+    });
+  },
+  getComments: (slug) => {
+    const info = get().artPiecesInfo.find((piece) => piece.slug === slug);
+    return info ? info.comments : [];
   },
 }));
